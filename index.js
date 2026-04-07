f739bcb13bec6e43de30bcf821d58238cac92cf9
Nezeri <ichigvintsev@icloud.com>
2026-04-07 17:59:15 +0500

    2

--- index.js
+++ index.js
@@ -5,3 +5,15 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const openModalBtn = document.getElementsByClassName('buttonOpenModal')[0];
+const closeModalBtn = document.getElementsByClassName('buttonCloseModal')[0];
+const modalOverlay = document.getElementsByClassName('modalOverlay')[0];
+
+openModalBtn.addEventListener('click', function () {
+    modalOverlay.classList.add('active');
+});
+
+closeModalBtn.addEventListener('click', function () {
+    modalOverlay.classList.remove('active');
+});
