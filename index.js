2cb9c2fd6d72765bec0f5f02b4678d24f5f7b54c
Nezeri <ichigvintsev@icloud.com>
2026-04-07 18:28:28 +0500

    3-4

--- index.js
+++ index.js
@@ -6,14 +6,41 @@
     element.style.width = '300px';
 */
 
-const openModalBtn = document.getElementsByClassName('buttonOpenModal')[0];
-const closeModalBtn = document.getElementsByClassName('buttonCloseModal')[0];
-const modalOverlay = document.getElementsByClassName('modalOverlay')[0];
+const openModalBtn = document.querySelector('.buttonOpenModal');
+const closeModalBtn = document.querySelector('.buttonCloseModal');
+const modalOverlay = document.querySelector('.modalOverlay');
+const progressClip = document.querySelector('.progressClip');
+
+const DURATION = 3000;
+let animationId = 0;
+
+function setProgress(value) {
+    progressClip.style.clipPath = 'inset(0 ' + (100 - value * 100) + '% 0 0)';
+}
+
+function animateProgressBar() {
+    cancelAnimationFrame(animationId);
+    setProgress(0);
+    const startTime = performance.now();
+
+    function frame(currentTime) {
+        const progress = Math.min((currentTime - startTime) / DURATION, 1);
+        setProgress(progress);
+
+        if (progress < 1) {
+            animationId = requestAnimationFrame(frame);
+        }
+    }
+
+    animationId = requestAnimationFrame(frame);
+}
 
 openModalBtn.addEventListener('click', function () {
     modalOverlay.classList.add('active');
+    animateProgressBar();
 });
 
 closeModalBtn.addEventListener('click', function () {
     modalOverlay.classList.remove('active');
+    cancelAnimationFrame(animationId);
 });
